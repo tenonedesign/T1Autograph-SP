71e669dccfc5a7ce36e019c806ef1a83e6670520
Ten One Design <peter@tenonedesign.com>
2021-01-15 16:48:57 -0500

    -

--- Package.swift
+++ Package.swift
@@ -4,25 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "T1Autograph-SP",
+    name: "T1Autograph",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "T1Autograph-SP",
-            targets: ["T1Autograph-SP"]),
+            name: "T1Autograph",
+            targets: ["T1Autograph"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "T1Autograph-SP",
-            dependencies: []),
-        .testTarget(
-            name: "T1Autograph-SPTests",
-            dependencies: ["T1Autograph-SP"]),
+        .binaryTarget(
+            name: "T1Autograph",
+            url: "https://tenonedesign.com/media/T1Autograph-2.0.5.zip",
+            // reset cache if this needs to change:
+            // In Xcode's File menu, select 'Swift Packages' and then 'Reset Package Caches'
+            checksum: "1f184efbcbac1588be6efce6875c0a2ff94c71ff84bc9ba4d7e4e1cf4e0faf2d"
+        ),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# T1Autograph-SP
+# T1Autograph
 
-A description of this package.
+Simple signature capture using a built-in modal view or a view of your choice.
+
+Integrate smooth signature capture into your iOS app, with built-in support for Apple Pencil and 3D Touch. It's simple to use, inexpensive, and has dead easy integration with Xcode. A demo application is available at the [T1Autograph page](https://tenonedesign.com/t1autograph).
